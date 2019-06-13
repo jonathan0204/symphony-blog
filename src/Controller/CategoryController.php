efe0cdaaadafe52b01766c8aa3215974c2292f40
jonathan saintclemente <jonathansaintclemente@gmail.com>
2019-06-13 17:54:06 +0200

    Symfony : Sécurisons nos routes.

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -1,99 +1,42 @@
 <?php
-
-namespace App\Controller;
-
-use App\Entity\Category;
-use App\Form\Category1Type;
-use App\Repository\CategoryRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-
-/**
- * @IsGranted("ROLE_ADMIN")
- * @Route("/category")
- */
-class CategoryController extends AbstractController
-{
 	
-    /**
-     * @Route("/", name="category_index", methods={"GET"})
-     */
-    public function index(CategoryRepository $categoryRepository): Response
-    {
-        return $this->render('category/index.html.twig', [
-            'categories' => $categoryRepository->findAll(),
-        ]);
-    }
-
-    /**
-     * @Route("/new", name="category_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $category = new Category();
-        $form = $this->createForm(Category1Type::class, $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($category);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('category_index');
-        }
-
-        return $this->render('category/new.html.twig', [
-            'category' => $category,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="category_show", methods={"GET"})
-     */
-    public function show(Category $category): Response
-    {
-        return $this->render('category/show.html.twig', [
-            'category' => $category,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="category_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Category $category): Response
-    {
-        $form = $this->createForm(Category1Type::class, $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('category_index', [
-                'id' => $category->getId(),
-            ]);
-        }
-
-        return $this->render('category/edit.html.twig', [
-            'category' => $category,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="category_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Category $category): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($category);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('category_index');
-    }
-}
+	namespace App\Controller;
+	
+	use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+	use Symfony\Component\Routing\Annotation\Route;
+	use Symfony\Component\HttpFoundation\Request;
+	use App\Form\CategoryType;
+	use App\Entity\Category;
+	
+	/**
+	 * Class CategoryController
+	 * @package App\Controller
+	 */
+	class CategoryController extends AbstractController
+	{
+		/**
+		 * @Route("/category", name="category")
+		 * @IsGranted("ROLE_ADMIN")
+		 */
+		public function add(Request $request)
+		{
+			
+			
+			$cat = new Category();
+			$form = $this->createForm(CategoryType::class, $cat);
+			$form->handleRequest($request);
+			
+			if($form->isSubmitted()){
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($cat);
+				$em->flush();
+				return $this->redirectToRoute('category');
+			}
+			
+			
+			return $this->render('category/index.html.twig', [
+				'my_form' => $form->createView(),
+			]);
+		}
+	}
